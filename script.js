e18d492ad9d8c837d2e3eee053f837eeb2325ce0
Rebecca Ayipeh <rebeccaayipeh6@gmail.com>
2024-10-21 13:11:39 +0000

    init

---
+++ script.js
@@ -0,0 +1,66 @@
+let contact = [
+  {
+    id: 1,
+    name: "icecream",
+    email: "icecream@gmail.com",
+    phone: "0240978859",
+  },
+  {
+    id: 2,
+    name: "milo",
+    email: "milo@gmail.com",
+    phone: "0240978859",
+  },
+  {
+    id: 3,
+    name: "ice",
+    email: "ice@gmail.com",
+    phone: "0549570938",
+  },
+  {
+    id: 4,
+    name: "water",
+    email: "water@gmail.com",
+    phone: "0241114577",
+  },
+  {
+    id: 5,
+    name: "drink",
+    email: "drink@gmail.com",
+    phone: "0240978859",
+  },
+  {
+    id: 6,
+    name: "toffie",
+    email: "toffie@gmail.com",
+    phone: "0547609557",
+  },
+  {
+    id: 7,
+    name: "biscuit",
+    email: "biscuit@gmail.com",
+    phone: "0538390950",
+  },
+  {
+    id: 8,
+    name: "kivo",
+    email: "kivo@gmail.com",
+    phone: "0240978859",
+  },
+  {
+    id: 9,
+    name: "milk",
+    email: "milk@gmail.com",
+    phone: "0240978858",
+  },
+  {
+    id: 10,
+    name: "coke",
+    email: "coke@gmail.com",
+    phone: "0208678856",
+  },
+];
+
+for (let i = 0; i < contact.length; i++) {
+  console.log(contact[i]);
+}
